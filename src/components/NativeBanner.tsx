e8dd7fa9a0692435e9fe51a36f29b956952c695c
mariabernardop63-cell <mariabernardop63@gmail.com>
2026-01-19 20:19:14 +0200

    Update NativeBanner.tsx

--- src/components/NativeBanner.tsx
+++ src/components/NativeBanner.tsx
@@ -1,1 +1,29 @@
+import { useEffect, useRef } from 'react';
+
+const NativeBanner = () => {
+  const bannerRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const script = document.createElement('script');
+    script.src = "https://pl28517691.effectivegatecpm.com/878e4233857120a9a5ad99a5f373021e/invoke.js";
+    script.async = true;
+    script.setAttribute('data-cfasync', 'false');
+
+    if (bannerRef.current) {
+      bannerRef.current.appendChild(script);
+    }
+
+    return () => {
+      if (bannerRef.current) {
+        bannerRef.current.innerHTML = '';
+      }
+    };
+  }, []);
+
+  return (
+    <div id="container-878e4233857120a9a5ad99a5f373021e" ref={bannerRef}></div>
+  );
+};
+
+export default NativeBanner;
 
